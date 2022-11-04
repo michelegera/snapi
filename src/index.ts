f453905570294d0b67173ab1b74932d1585a6e54
Michele Gerarduzzi <hello@michelegera.dev>
2022-11-04 17:08:15 +0100

    Change port env variable name

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { createServer } from "@graphql-yoga/node";
 
 import { schema } from "./schema";
 
-const port = Number(process.env.API_PORT) || 4000;
+const port = Number(process.env.PORT) || 4000;
 const server = createServer({ port, schema });
 
 server.start();
