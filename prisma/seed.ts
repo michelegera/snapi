c48ad0acbcb52344f1a562dc1268c08c8782e8b9
Michele Gerarduzzi <hello@michelegera.dev>
2022-11-05 14:34:20 +0100

    Move card seed data to JSON files

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,62 +1,22 @@
 import { PrismaClient } from "@prisma/client";
 
+import base from "./data/cards/base.json";
+import pool1 from "./data/cards/pool_1.json";
+
 const prisma = new PrismaClient();
 
 async function main() {
   // Delete all cards
   await prisma.card.deleteMany({});
 
-  // Create some cards
-  await prisma.card.create({
-    data: {
-      name: "Jessica Jones",
-      type: "character",
-      cost: 4,
-      power: 4,
-      ability: "If you don’t play a card here next turn, +4 Power.",
-      trigger: "on_reveal",
-    },
-  });
-  await prisma.card.create({
-    data: {
-      name: "Mister Fantastic",
-      type: "character",
-      cost: 3,
-      power: 2,
-      ability: "Adjacent locations have +2 Power.",
-      trigger: "ongoing",
-    },
-  });
-  await prisma.card.create({
-    data: {
-      name: "Nightcrawler",
-      type: "character",
-      cost: 1,
-      power: 2,
-      ability: "You can move this once.",
-    },
-  });
-  await prisma.card.create({
-    data: {
-      name: "Odin",
-      type: "character",
-      cost: 6,
-      power: 8,
-      ability:
-        "Activate the On Reveal ability of your other cards at this location.",
-      trigger: "on_reveal",
-    },
-  });
-  await prisma.card.create({
-    data: {
-      name: "Spectrum",
-      type: "character",
-      cost: 6,
-      power: 5,
-      ability: "Give your Ongoing cards +2 Power.",
-      trigger: "on_reveal",
-    },
-  });
+  // Create cards
+  await Promise.all(
+    [...base, ...pool1].map((card) => {
+      return prisma.card.create({
+        data: card,
+      });
+    })
+  );
 }
 
 main();
