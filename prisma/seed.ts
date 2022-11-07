b66870d487beb3cdef2c45c35ad278e571e7b694
Michele Gerarduzzi <hello@michelegera.dev>
2022-11-07 20:40:16 +0100

    [wip] Seed locations

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,17 +1,19 @@
 import { PrismaClient } from "@prisma/client";
 
+// Cards
 import starter from "./data/cards/starter.json";
 import pool0 from "./data/cards/pool_0.json";
 import pool1 from "./data/cards/pool_1.json";
 import pool2 from "./data/cards/pool_2.json";
 
+// Locations
+import locations from "./data/locations/locations.json";
+
 const prisma = new PrismaClient();
 
 async function main() {
-  // Delete all cards
+  // Seed Cards
   await prisma.card.deleteMany({});
-
-  // Create cards
   await Promise.all(
     [
       ...starter,
@@ -21,6 +23,16 @@ async function main() {
     ].map((card) => {
       return prisma.card.create({
         data: card,
+      });
+    })
+  );
+
+  // Seed Locations
+  await prisma.location.deleteMany({});
+  await Promise.all(
+    locations.map((location) => {
+      return prisma.location.create({
+        data: location,
       });
     })
   );
