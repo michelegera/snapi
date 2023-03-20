3d74b2edeb9358220f6fde7d38dacd2681e01cf4
Michele Gerarduzzi <hello@michelegera.dev>
2023-03-20 16:03:14 +0100

    Add Location model to schema

---
+++ src/models/Location.ts
@@ -0,0 +1,21 @@
+import { builder } from "../builder";
+import { prisma } from "../db";
+
+builder.prismaObject("Location", {
+  fields: (t) => ({
+    id: t.exposeID("id"),
+    name: t.exposeString("name"),
+    description: t.exposeString("description"),
+    rarity: t.exposeString("rarity"),
+    difficulty: t.exposeString("difficulty"),
+  }),
+});
+
+builder.queryField("locations", (t) =>
+  t.prismaField({
+    type: ["Location"],
+    resolve: async (query) => {
+      return prisma.location.findMany({ ...query });
+    },
+  }),
+);
--- src/schema.ts
+++ src/schema.ts
@@ -1,6 +1,6 @@
-import exp from "constants";
 import { builder } from "./builder";
 
 import "./models/Card";
+import "./models/Location";
 
 export const schema = builder.toSchema({});
